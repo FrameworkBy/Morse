dc827fe92db6afb3cc05c18bdc8f8f43d81e224e
EvgenijPrav <praviloffevg@gmail.com>
2016-06-15 10:50:29 +0300

    Added analyticstracking

---
+++ analyticstracking.php
@@ -0,0 +1,63 @@
+<?php if('80.94.162.66' == $_SERVER["REMOTE_ADDR"] or '80.94.162.88' == $_SERVER["REMOTE_ADDR"] or '80.94.171.87' == $_SERVER["REMOTE_ADDR"] or '192.168.94.85' == $_SERVER["REMOTE_ADDR"] or '192.168.95.1' == $_SERVER["REMOTE_ADDR"] or '127.0.0.1' == $_SERVER["REMOTE_ADDR"]){ ?>
+	<script src="https://www.kuoll.com/cdn/embedScript/prod/bootloader.js"></script>
+	<script>
+		kuoll.startRecord({
+			API_KEY: "a30da3a300213397",
+			userId: 1, // userId in your system, optional
+			localRecording: false // enable to not save data to server until issue is created
+		});
+	</script>
+<?php } ?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71873630-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter35956270 = new Ya.Metrika({
+                    id:35956270,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<!--<noscript><div><img src="https://mc.yandex.ru/watch/35956270" style="position:absolute; left:-9999px;" alt="" /></div></noscript>-->
+<!-- /Yandex.Metrika counter -->
+
+<!--Akavita counter start-->
+<script type="text/javascript">var AC_ID=64455;var AC_TR=false;
+(function(){var l='http://adlik.akavita.com/acode.js'; var t='text/javascript';
+try {var h=document.getElementsByTagName('head')[0];
+var s=document.createElement('script'); s.src=l;s.type=t;h.appendChild(s);}catch(e){
+document.write(unescape('%3Cscript src="'+l+'" type="'+t+'"%3E%3C/script%3E'));}})();
+</script><!--<span id="AC_Image"></span>
+<noscript><a target='_blank' href='http://www.akavita.by/'>
+<img src='http://adlik.akavita.com/bin/lik?id=64455&it=1'
+border='0' height='1' width='1' alt='Akavita'/>
+</a></noscript>-->
+<!--Akavita counter end-->
